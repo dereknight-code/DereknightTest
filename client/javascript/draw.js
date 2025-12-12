81a99b11534ddc7a2633da98d4eb84a148161e66
dreamderek <dreamderek@gmail.com>
2025-12-12 17:57:49 +0800

    比擦

--- client/javascript/draw.js
+++ client/javascript/draw.js
@@ -1,68 +1,158 @@
 class Draw {
     constructor(panel) {
+        this.EPS = 1e-10;
+        this.ONE_MINUS_EPS = 1 - this.EPS;
+
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         panel.appendChild(this.canvas);
 
-        this.shapes = [];   // 儲存畫過的線段
+        this.shapes = [];
 
-        this.observer = new ResizeObserver(() => this.resize());
+        this.observer = new ResizeObserver(() => this.#resize());
         this.observer.observe(panel);
 
-        this.resize();
+        this.#resize();
     }
 
-    resize() {
+    #resize() {
         const rect = this.canvas.parentElement.getBoundingClientRect();
         this.canvas.width = rect.width;
         this.canvas.height = rect.height;
-        this.redraw();
+        this.#redraw();
     }
 
-    redraw() {
-        this.clear();
+    #redraw() {
+        this.#clear();
+        // console.log(this.shapes);
         for (const s of this.shapes) {
             const pts = s.point;
-
             if (!pts || pts.length < 2) continue;
 
             this.ctx.strokeStyle = s.color || 'black';
             this.ctx.lineWidth = s.width || 1;
-            this.ctx.beginPath();
-            this.ctx.moveTo(pts[0][0], pts[0][1]);
-            for (let i = 1; i < pts.length; i++) {
-                this.ctx.lineTo(pts[i][0], pts[i][1]);
+
+            switch (s.toolstyle || '1') {
+                case '0':
+                    this.#redraw0();
+                    break;
+                case '1':
+                    this.#redraw1(pts);
+                    break;
+                case '2':
+                    this.#redraw2(pts);
+                    break;
+                default: { }
             }
-            this.ctx.stroke();
         }
+
+        const lastIdx = this.shapes.length - 1;
+        this.shapes = this.shapes.filter((item, index) => {
+            if (index === lastIdx) return true;
+            if (item.toolstyle == '0') return false;
+            return true;
+        });
+    }
+    #redraw0() {
+        const lastIdx = this.shapes.length - 1;
+        const last = this.shapes[lastIdx];
+        if (!last || last.point.length < 2) return;
+
+        const p2 = last.point[last.point.length - 1];
+        const p1 = last.point[last.point.length - 2];
+
+        this.shapes = this.shapes.filter((item, index) => {
+            if (index === lastIdx) return true;
+            let hasCollision = false;
+            for (let i = 0; i < item.point.length - 1; i++) {
+                const sp = item.point[i];
+                const ep = item.point[i + 1];
+
+                if (this.#cross(p1, p2, sp, ep)) {
+                    hasCollision = true;
+                    break;
+                }
+            }
+            return !hasCollision;
+        });
     }
 
-    conline(x, y) {
+
+    #redraw1(pts) {
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(pts[0][0], pts[0][1]);
+        for (let i = 1; i < pts.length; i++) {
+            this.ctx.lineTo(pts[i][0], pts[i][1]);
+        }
+        this.ctx.stroke();
+    }
+    #redraw2(pts) {
+        this.ctx.lineCap = 'round';
+        this.ctx.lineJoin = 'round';
+        this.ctx.beginPath();
+        this.ctx.moveTo(pts[0][0], pts[0][1]);
+        for (let i = 1; i < pts.length - 2; i++) {
+            const xc = (pts[i][0] + pts[i + 1][0]) / 2;
+            const yc = (pts[i][1] + pts[i + 1][1]) / 2;
+            this.ctx.quadraticCurveTo(pts[i][0], pts[i][1], xc, yc);
+        }
+        const len = pts.length;
+        this.ctx.quadraticCurveTo(
+            pts[len - 2][0],
+            pts[len - 2][1],
+            pts[len - 1][0],
+            pts[len - 1][1]
+        );
+
+        this.ctx.stroke();
+    }
+
+    #clear() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    #cross(a, p, b, c) {
+        if ((a[0] === b[0] && a[1] === b[1]) || (a[0] === c[0] && a[1] === c[1])) return true;
+
+        const abx = b[0] - a[0], aby = b[1] - a[1];
+        const acx = c[0] - a[0], acy = c[1] - a[1];
+        const apx = p[0] - a[0], apy = p[1] - a[1];
+
+        const det = abx * acy - aby * acx;
+
+        if (Math.abs(det) < this.EPS) {
+            return ((abx > 0) !== (acx > 0)) || ((aby > 0) !== (acy > 0));
+        }
+
+        const x = (apx * acy - apy * acx) / det;
+        if (x < -this.EPS) return false;
+
+        const y = (abx * apy - aby * apx) / det;
+        if (y < -this.EPS) return false;
+
+        return (x + y >= this.ONE_MINUS_EPS);
+    }
+
+    addpoint(x, y) {
         const last = this.shapes.pop();
         last.point.push([x, y])
         this.line(last);
+        this.#redraw();
     }
 
     line(shape = {
         point: [[50, 50], [200, 200], [300, 100]],
         color: 'red',
-        width: 5
+        width: 5,
+        toolstyle: 1
     }) {
         this.shapes.push(shape);
-        this.redraw();
-        console.log(this.shapes);
     }
-
-    clear() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-
 
 
     getRelativePos(e) {
         const rect = this.canvas.getBoundingClientRect();
-
-        // 避免 retina、CSS scale 造成偏差
         const scaleX = this.canvas.width / rect.width;
         const scaleY = this.canvas.height / rect.height;
 
--- client/javascript/windows.js
+++ client/javascript/windows.js
@@ -1,7 +1,10 @@
+document.addEventListener('gesturestart', (e) => e.preventDefault());
+document.addEventListener('gesturechange', (e) => e.preventDefault());
+document.addEventListener('gestureend', (e) => e.preventDefault());
 
 document.addEventListener('contextmenu', function (e) {
     e.preventDefault();
-    
+
     menu.style.left = e.pageX + "px";
     menu.style.top = e.pageY + "px";
     createMenu(true);
@@ -11,6 +14,17 @@ document.addEventListener('click', function (e) {
     createMenu(false);
 });
 
+document.addEventListener('touchmove', (e) => {
+    if (e.touches.length > 1) {
+        e.preventDefault();  // 阻擋兩指縮放
+    }
+}, { passive: false });
+
+document.addEventListener("touchstart", function (e) {
+    if (e.touches.length > 1) {
+        e.preventDefault();   // 封鎖雙指縮放
+    }
+}, { passive: false });
 
 const menu = document.createElement('div');
 menu.id = 'custom-context-menu';
@@ -19,29 +33,43 @@ menu.style.backgroundColor = '#fff';
 menu.style.border = '1px solid #ccc';
 menu.style.padding = '10px';
 menu.style.boxShadow = '0 2px 10px rgba(0,0,0,0.2)';
+
 function createMenu(show) {
     if (show) {
         document.body.appendChild(menu);
-    } else if(document.body.contains(menu)) {
+    } else if (document.body.contains(menu)) {
         document.body.removeChild(menu);
     }
 }
 
+//====================================================================================
 
 
-document.addEventListener('gesturestart', (e) => e.preventDefault());
-document.addEventListener('gesturechange', (e) => e.preventDefault());
-document.addEventListener('gestureend', (e) => e.preventDefault());
+window.onload = () => {
+
+    const panel = document.getElementById('draw');
+    const toolstyle = document.querySelector("#toolstyle");
+    const tool = document.querySelectorAll(".button");
+
+    const draw = new Draw(panel);
+    //touchstart
+    panel.addEventListener('pointerdown', (e) => {
+        const pos = draw.getRelativePos(e);
+        draw.line({ point: [[pos.x, pos.y]], width: 1, toolstyle: toolstyle.innerHTML});
+    }, { passive: false });
+    //touchmove
+    panel.addEventListener('pointermove', (e) => {
+        if (e.pointerType === 'mouse' && e.buttons === 0) return;
+        const pos = draw.getRelativePos(e);
+        draw.addpoint(pos.x, pos.y);
+    }, { passive: false });
 
 
+    tool.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            toolstyle.innerHTML = index;
+        });
 
-let lastTouch = 0;
+    });
 
-document.addEventListener("touchstart", function (e) {
-    if (e.touches.length > 1) {
-        e.preventDefault();   // 封鎖雙指縮放
-    }
-}, { passive: false });
-
-
-document.addEventListener('contextmenu', e => e.preventDefault());
+}
